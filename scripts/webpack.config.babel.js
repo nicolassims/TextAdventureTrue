6164ede78db014074236f774cee326f7856063d5
nicolassims <sableye.nico@gmail.com>
2016-02-18 14:36:12 -0500

    Added eslintrc

--- scripts/webpack.config.babel.js
+++ scripts/webpack.config.babel.js
@@ -1,12 +1,12 @@
 module.exports = {
-     entry: "./main.es6",
-     output: {
-          path: __dirname,
-          filename: "bundle.js"
-     },
-     module: {
-          loaders: [
-               {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
-          ]
-     }
+  entry: './main.es6',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+         {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}
+    ]
+  }
 };
